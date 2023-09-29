fed7e71354096f0defc9a43ed4b16e4ee02a0a0e
Wes Stahler <Wes.Stahler@gmail.com>
2023-09-29 10:57:44 -0400

    Update ImportExcel examples

--- 3-ImportExcelDemo.ps1
+++ 3-ImportExcelDemo.ps1
@@ -7,6 +7,7 @@ break
     Check out his GitHub repo: https://github.com/dfinke
 #>
 
+#region - metadata about the module ##########################################################
 # Let's find the module
 Find-Module ImportExcel
 
@@ -21,10 +22,63 @@ Get-Module ImportExcel | Format-List
 
 # Explore the various cmdlets it has to offer
 Get-Command -Module ImportExcel | Out-GridView
+#endregion ##########################################################
 
+#region - Conditional Formatting ##########################################################
+# Let's kick out some data to Excel (simple example)
 Get-ADUser -Filter {samaccountname -like "Stah*"} -Properties Department, Title | 
 Select-Object Name, Enabled, Department, Title | Export-Excel
 
-#Export-Excel -Path C:\temp\users.xlsx -AutoSize -Show
+# Simple Conditional Formatting
+$disabled = New-ConditionalText False #White Red
+$enabled = New-ConditionalText True white Green
+Get-ADUser -Filter {samaccountname -like "Stah*"} -Properties Department, Title | 
+Select-Object Name, Enabled, Department, Title | Export-Excel -ConditionalText $disabled #, $enabled
+
+# Example of IconSets
+# https://gist.github.com/stahler/97bb228b9d43c3ba4f1194cb7ea95880#file-add-iconset-ps1
+
+# Example comparing groups
+$Mark = Get-ADPrincipalGroupMembership ambl01 | Sort Name | Select @{N='Mark';E={$PSItem.Name}}
+$Wes =  Get-ADPrincipalGroupMembership stah06 | Sort Name | Select @{N='Wes';E={$PSItem.Name}}
+
+$xlfile = "c:\temp\groups.xlsx"
+Remove-Item $xlfile -ErrorAction SilentlyContinue
+
+$wsName = "Unique Values"
+$c1 = New-ConditionalText -ConditionalType UniqueValues -Range '$A:$C' 
+
+$Mark | Export-Excel $xlfile -WorksheetName $wsName
+$Wes | Export-Excel $xlfile -WorksheetName $wsName -StartColumn 3 -ConditionalText $c1 -Show
+#endregion ##########################################################
+
+#region - Pivot tables ##########################################################
+#endregion ##########################################################
+
+#region - other examples##########################################################
+# adding sparklines - https://gist.github.com/stahler/7b5c0c8b6347b010bf9bce64b3c29326#file-add-sparkline-ps1
+# adding hyperlinks - https://gist.github.com/stahler/47b0842350f1177da8c26308809ee769
+# adding formulas - https://gist.github.com/stahler/51e84413e4c05d9a563dc7cac766b5cc
+# Keeping leading zeros - https://gist.github.com/stahler/72e36020b5c24d41ed9988f3f5a7dd48#file-keep-leadingzero-ps1
+
+# Example of creating multiple worksheets
+$groups =   'Domain Admins',
+            'Group Policy Creator Owners', 
+            'EEI-OU-ADMIN',
+            'accounts.change.limited',
+            'Level 2 Support'
+
+$dt = Get-Date -Format yyyyMMdd
+$path = "C:\temp\PriviledgedGroups$dt.xlsx"
+Remove-Item -Path $path -ErrorAction SilentlyContinue
+
+foreach ($group in $groups) {
+    Get-ADGroupMember $group | 
+    Select-Object SAMAccountName, Name, DistinguishedName | 
+    Export-Excel -Path $path -WorksheetName "$($group)" -AutoSize
+}
+
+Invoke-Item -Path $path
+#endregion ##########################################################
 
 
